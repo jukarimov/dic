700e7e41869d6e658c866ef26741acc75ed3004c
Jalil Karimov <jukarimov@gmail.com>
2012-06-14 23:44:04 +0500

    This is dic's parts 8-)

---
+++ index.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	char k[1000];
+	char v[100 * 1000];
+	char line[200 * 1000];
+	unsigned int i, j, vlen, klen, len, pf;
+
+	FILE *fp = fopen(argv[1], "r");
+
+	while (fgets(line, sizeof(line), fp)) {
+		if (strstr(line, "INSERT")) {
+			len = strlen(line) - 1;
+			line[len] = 0;
+			i = 0;
+			while (line[i++] != '\'' && i < len)
+				;
+			j = 0;
+			while (i < len && line[i] != '\'')
+				k[j++] = line[i++];
+			k[j] = 0;
+			klen = j;
+
+			i++;
+			while (i < len && line[i] != '\'')
+				i++;
+			i++;
+
+			j = 0;
+			while (i < len && line[i] != '\'')
+				v[j++] = line[i++];
+
+			v[j] = 0;
+			vlen = j;
+
+		} else {
+			printf("Expected INSERT, got %s\n", line);
+			exit(-1);
+		}
+
+		pf = ftell(fp) - vlen;
+		printf("%s:%d@%d\n", k, vlen + 1, pf - 4);
+	}
+	fclose(fp);
+
+	return 0;
+}
+
